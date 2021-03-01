98f63c8ababfaed4f682a8c1a05a9c730fa0ff05
Maciej Perzankowski <perzankowski.m2001@gmail.com>
2021-03-01 14:06:00 +0100

    first commit

---
+++ pages/USnews/[slug].js
@@ -0,0 +1,92 @@
+import styles from '../../styles/Home.module.css'
+import React from "react";
+import {Toolbar} from "../../components/toolbar";
+import {useRouter} from "next";
+import Router from "next/router";
+
+export const Feed = ({articles, pageNumber}) => {
+  const router = useRouter;
+  return articles.length ? (
+    <>
+      <div className="page-container">
+        <Toolbar/>
+
+        <div className={styles.main}>
+          {articles.map((article, index) => (
+            <div key={index} className={styles.post}>
+              <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+              <p>{article.description}</p>
+              {!!article.urlToImage && <img style={{borderRadius: "3%"}} src={article.urlToImage}/>}
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.paginator}>
+          <div
+            className={pageNumber === 1 ? styles.disabled : styles.active}
+            onClick={() => {
+              if (pageNumber > 1 && pageNumber !==undefined) {
+                Router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
+              }
+            }}
+          >
+            Previous Page
+          </div>
+
+          <div>#{pageNumber}</div>
+
+          <div
+            className={pageNumber === 5 ? styles.disabled : styles.active}
+            onClick={() => {
+              if (pageNumber < 5 && pageNumber !==undefined) {
+                Router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
+              }
+            }}
+          >
+            Next Page
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
+    <div className="page-container">
+      <Toolbar/>
+      <div className={styles.main}>
+        <h1>Oops! No articles for this page</h1>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async pageContext => {
+  const pageNumber = pageContext.query.slug;
+
+  if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
+    return {
+      props: {
+        articles: [],
+        pageNumber: 1,
+      },
+    };
+  }
+
+  const apiResponse = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
+    },
+  ).then(res => res.json());
+
+  const {articles} = apiResponse;
+
+  return {
+    props: {
+      articles: articles,
+      pageNumber: Number.parseInt(pageNumber),
+    },
+  };
+};
+
+export default Feed;
